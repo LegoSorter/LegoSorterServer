a58602294727ae539b2385b9bdda229915e84c24
kzawora <konrad.zawora@outlook.com>
2020-12-19 02:27:35 +0100

    update protobuf

--- lego_sorter_server/generated/LegoBrick_pb2_grpc.py
+++ lego_sorter_server/generated/LegoBrick_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import lego_sorter_server.generated.LegoBrick_pb2 as LegoBrick__pb2
+import LegoBrick_pb2 as LegoBrick__pb2
 
 
 class LegoBrickStub(object):
