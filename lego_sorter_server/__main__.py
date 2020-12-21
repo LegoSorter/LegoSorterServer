ba0f69f1654bfbdf525c49870e79b0d8a9c4e526
kzawora <konrad.zawora@outlook.com>
2020-12-21 12:13:56 +0100

    change logging level to INFO

--- lego_sorter_server/__main__.py
+++ lego_sorter_server/__main__.py
@@ -2,5 +2,5 @@ from lego_sorter_server.server import Server
 import logging
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.INFO)
     Server.run()
