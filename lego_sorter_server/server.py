08877928df974180542e90529684104bf21e0202
Yas <bartosz12364@gmail.com>
2020-12-21 14:41:57 +0100

    Fix max_receive_message issue

--- lego_sorter_server/server.py
+++ lego_sorter_server/server.py
@@ -9,7 +9,8 @@ class Server:
 
     @staticmethod
     def run():
-        server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+        options = [('grpc.max_receive_message_length', 100 * 1024 * 1024)]
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=1), options=options)
         LegoBrick_pb2_grpc.add_LegoBrickServicer_to_server(LegoBrickService(), server)
         server.add_insecure_port('[::]:50051')
         server.start()
