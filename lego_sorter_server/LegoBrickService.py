0043cf80bae5e68e99b5285f16760f91e5f62c9a
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-02-28 13:58:13 +0100

    Split method for detecting and classifying bricks into detect only and detectAndClassify

--- lego_sorter_server/LegoBrickService.py
+++ lego_sorter_server/LegoBrickService.py
@@ -1,4 +1,5 @@
 from concurrent import futures
+from typing import List
 
 from lego_sorter_server.classifier.LegoClassifierRunner import LegoClassifierRunner
 from lego_sorter_server.detection.DetectionUtils import crop_with_margin
@@ -23,7 +24,7 @@ class LegoBrickService(LegoBrick_pb2_grpc.LegoBrickServicer):
         self.detector = LegoDetectorProvider.get_default_detector()
         self.storage = LegoImageStorage()
         self.classifier = LegoClassifierRunner()
-        self.classifier.load_trained_model()
+        # self.classifier.load_trained_model()
         self.processing_queue = ImageProcessingQueue()
         self.detection_runner = LegoDetectionRunner(self.processing_queue, self.detector, self.storage)
         self.executor = futures.ThreadPoolExecutor(max_workers=4)
@@ -45,12 +46,6 @@ class LegoBrickService(LegoBrick_pb2_grpc.LegoBrickServicer):
 
         return image
 
-    def RecognizeLegoBrickInImage(self, request: LegoImage, context):
-        image = self._prepare_image(request)
-        self.storage.save_image(image, "unknown")
-
-        return Empty()
-
     def CollectCroppedImages(self, request: LegoImageStore, context):
         self.executor.submit(self._handle_collect_cropped_images, request)
 
@@ -66,17 +61,16 @@ class LegoBrickService(LegoBrick_pb2_grpc.LegoBrickServicer):
 
         return Empty()
 
-    def DetectBricks(self, request: LegoImage, context):
+    def _detect_bricks(self, request: LegoImage) -> List[BoundingBox]:
         image = self._prepare_image(request)
         width, height = image.size
         image_resized, scale = DetectionUtils.resize(image, 640)  # image.resize((640, 640), 0)
         detections = self.detector.detect_lego(np.array(image_resized))
 
-        bbs_with_blobs = []
+        bbs = []
         for i in range(len(detections['detection_classes'])):
             if detections['detection_scores'][i] < 0.5:
-                # continue # IF NOT SORTED
-                break  # IF SORTED
+                continue
 
             bb = BoundingBox()
 
@@ -84,10 +78,26 @@ class LegoBrickService(LegoBrick_pb2_grpc.LegoBrickServicer):
             if bb.ymax >= height or bb.xmax >= width:
                 continue
 
-            cropped_brick = crop_with_margin(image, bb.ymin, bb.xmin, bb.ymax, bb.xmax)
-
             bb.score = detections['detection_scores'][i]
             bb.label = 'lego'
+            bbs.append(bb)
+
+        return bbs
+
+    def DetectBricks(self, request: LegoImage, context):
+        bbs = self._detect_bricks(request)
+        bb_list = ListOfBoundingBoxes()
+        bb_list.packet.extend(bbs)
+        return bb_list
+
+    def DetectAndClassifyBricks(self, request, context):
+        bbs = self._detect_bricks(request)
+        image = self._prepare_image(request)
+
+        bbs_with_blobs = []
+
+        for bb in bbs:
+            cropped_brick = crop_with_margin(image, bb.ymin, bb.xmin, bb.ymax, bb.xmax)
             bbs_with_blobs.append((bb, cropped_brick))
 
         bbs_labels = self.classifier.predict_from_pil([img for _, img in bbs_with_blobs])
