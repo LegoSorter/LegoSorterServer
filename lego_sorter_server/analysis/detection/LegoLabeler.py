062c41005bf043d48ca5f7494e6e9556db6e8968
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-04-23 00:25:55 +0200

    Fix lego labeler, swap x with y

--- lego_sorter_server/analysis/detection/LegoLabeler.py
+++ lego_sorter_server/analysis/detection/LegoLabeler.py
@@ -1,8 +1,8 @@
 class LegoLabeler:
 
-    def to_label_file(self, filename, path, image_width, image_height, bbs_xyxy_array):
+    def to_label_file(self, filename, path, image_width, image_height, bbs_yxyx_array):
         objects = ""
-        for coord in bbs_xyxy_array:
+        for coord in bbs_yxyx_array:
             objects += self.get_object(*coord)
 
         return f"""<annotation>
@@ -22,7 +22,7 @@ class LegoLabeler:
         </annotation>"""
 
     @staticmethod
-    def get_object(x1, y1, x2, y2):
+    def get_object(y1, x1, y2, x2):
         return f"""<object>
                         <name>lego</name>
                         <pose>Unspecified</pose>
