7fa8acb305f62a6f1fe7f0c3a8a3479c7cc564c2
Derflin <cubussledz1@gmail.com>
2023-09-04 14:35:16 +0200

    Updated config file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Used in SortingProcessor.py
-CLASSIFICATION_IN_ROW_MIN_COUNT = 5
+CLASSIFICATION_IN_ROW_MIN_COUNT = 10
 
 # Used in LegoSorterController.py
 CONVEYOR_LOCAL_ADDRESS = "http://192.168.83.45:8000/"
@@ -7,3 +7,7 @@ SORTER_LOCAL_ADDRESS = "http://192.168.83.45:8001/"
 
 # Used in SimpleOrdering.py
 BORDER_MARGIN = 5
+
+# Used in classification/classifiers/KerasLabelClassifier.py
+CLASSIFICATION_LABEL_COUNT = 5
+CLASSIFICATION_PROBABILITY_MARGIN = 0.01
